aa5c5cd7c56807193bf9ce079019e88a5eda52f7
Roman Mukhin <roman11mukhin@gmail.com>
2013-05-27 00:38:23 +0300

    Update knowledge.rb

--- knowledge.rb
+++ knowledge.rb
@@ -1,24 +1,15 @@
 class Module
-  def attribute(arg,&block)
-    
-    argument, default= (arg.class == Hash ? [arg.keys.first,arg.values.first] : [arg,nil])
-    var = "@#{argument}".to_sym
-    instance_variable_set(var,default) 
-
-    self.send :define_method, "#{argument}" do  
-      if instance_variables.include?(var)
-        return instance_variable_get var 
-      else
-        return block_given? ? instance_eval(&block) : default
+  def attribute(name, value = nil, &block)
+    if name.is_a? Hash
+      name.each {|key,value| attribute(key,value)}
+    else      
+      attr_writer name     
+      define_method("#{name}") do 
+        instance_variable_defined?("@#{name}") ? instance_variable_get("@#{name}") : (block_given? ? instance_eval(&block) : value)
+      end     
+      define_method("#{name}?") do
+        !!send(name)
       end
     end
-
-    self.send :define_method, "#{argument}=" do |num| 
-      instance_variable_set(var, num)
-    end
-
-    self.send :define_method, "#{argument}?" do
-      !self.send("#{argument}").nil? 
-    end  
   end
 end
